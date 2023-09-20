5fbcdb32ad2687651d49784232ebb279114ff874
luccaovf <114105607+luccaovf@users.noreply.github.com>
2023-09-20 12:04:05 -0300

    Começo do grafo
    Teste para ver se so pelo upload vai funcionar

---
+++ codigo/grafoProgMod/src/packMain/Vertice.java
@@ -0,0 +1,20 @@
+package packMain;
+
+public class Vertice {
+	private int id;
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
