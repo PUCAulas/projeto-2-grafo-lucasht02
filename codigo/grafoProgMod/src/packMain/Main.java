e1124e60f41239b1fbd3f41d015ed4c4d5fa8415
Pedro De Campos Faria <pedroafonso939@yahoo.com.br>
2023-09-27 12:08:55 -0300

    Criando menu

--- codigo/grafoProgMod/src/packMain/Main.java
+++ codigo/grafoProgMod/src/packMain/Main.java
@@ -1,10 +1,35 @@
 package packMain;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		Scanner input = new Scanner(System.in);
+		int opcaoSelecionda = 999;
+        
+
+		System.out.println("----------------------");
+		System.out.println("Selecione a opcao que deseja: ");
+		System.out.println(" 1 - Adicionar vertice\n 2 - Adicinar arestas \n 3 - Imprimir Grafo");
+		System.out.println("----------------------");
+
+		opcaoSelecionda = input.nextInt();
+		while (opcaoSelecionda != 0) {
+			switch (opcaoSelecionda) {
+				case 1:
+					break;
+
+				case 2:
+					break;
+
+				case 3:
+					break;
+
+			}
+		}
 	}
 
 }
