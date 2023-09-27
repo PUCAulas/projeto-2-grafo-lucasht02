b4fde675f2ca5c51026dc27e2c6814c53ded6395
Pedro De Campos Faria <pedroafonso939@yahoo.com.br>
2023-09-27 11:43:23 -0300

    Update Aresta.java

--- codigo/grafoProgMod/src/packMain/Aresta.java
+++ codigo/grafoProgMod/src/packMain/Aresta.java
@@ -1,28 +1,49 @@
-package packMain;
-
 public class Aresta {
-	private int idOrigem;
-	private int idDestino;
-	private int peso;
-	
-	public int getIdOrigem() {
-		return idOrigem;
-	}
-	public void setIdOrigem(int idOrigem) {
-		this.idOrigem = idOrigem;
-	}
-	public int getIdDestino() {
-		return idDestino;
-	}
-	public void setIdDestino(int idDestino) {
-		this.idDestino = idDestino;
-	}
-	public int getPeso() {
-		return peso;
-	}
-	public void setPeso(int peso) {
-		this.peso = peso;
-	}
-	
-	
+
+    private Vertice vertice; // guardando o vertice e mostrando para onde esta apontando
+
+    private boolean direcao;
+
+    public Aresta(Vertice vertice, boolean direcao) {
+        this.vertice = vertice;
+        this.direcao = direcao;
+    }
+
+    public boolean direcao() {
+        return this.direcao;
+    }
+
+    public Vertice vertice() {
+        return this.vertice;
+    }
+
 }
+
+// package packMain;
+
+// public class Aresta {
+// 	private int idOrigem;
+// 	private int idDestino;
+// 	private int peso;
+	
+// 	public int getIdOrigem() {
+// 		return idOrigem;
+// 	}
+// 	public void setIdOrigem(int idOrigem) {
+// 		this.idOrigem = idOrigem;
+// 	}
+// 	public int getIdDestino() {
+// 		return idDestino;
+// 	}
+// 	public void setIdDestino(int idDestino) {
+// 		this.idDestino = idDestino;
+// 	}
+// 	public int getPeso() {
+// 		return peso;
+// 	}
+// 	public void setPeso(int peso) {
+// 		this.peso = peso;
+// 	}
+	
+	
+// }
